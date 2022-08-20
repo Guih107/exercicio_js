4e9ac97b8687d9676b3d81f99469af697714b610
Guih107 <guih107@gmail.com>
2022-08-20 14:08:45 -0300

    exercicio 1

---
+++ exercicios4/1.js
@@ -0,0 +1,26 @@
+//exercicio 1 //
+
+const a = 10;
+const b = 11;
+
+console.log('adição: ', a+b);
+
+const c = 10;
+const d = 5;
+
+console.log('subtração: ', c-d);
+
+const e = 15;
+const f = 2;
+
+console.log('multiplicação: ', e*f);
+
+const g = 10;
+const h = 5;
+
+console.log('divisão: ', g/h);
+
+const i = 15;
+const j = 2;
+
+console.log('resto: ', i % j);
