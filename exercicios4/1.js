84a5b85a5c976ca000c43534cad379913b7fa374
Guih107 <guih107@gmail.com>
2022-08-21 11:49:54 -0300

    segundo exercicio

--- exercicios4/1.js
+++ exercicios4/1.js
@@ -1,6 +1,6 @@
 //exercicio 1 //
 
-const a = 10;
+/*const a = 10;
 const b = 11;
 
 console.log('adição: ', a+b);
@@ -23,4 +23,15 @@ console.log('divisão: ', g/h);
 const i = 15;
 const j = 2;
 
-console.log('resto: ', i % j);
+console.log('resto: ', i % j);*/
+
+//exercicio 2 //
+
+const num1 = 34;
+const num2 = 52;
+
+if (num1 > num2){
+    console.log(num1);
+} else{
+    console.log(num2);
+}
